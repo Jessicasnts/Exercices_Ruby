37120808b89bb31379cf078f930fa69ba925800a
Jessicasnts <jessica.dasantos2@gmail.com>
2021-10-05 14:58:42 +0200

    ajout fichiers 13_14

--- exo_13.rb
+++ exo_13.rb
@@ -1,2 +1,9 @@
-tab = [1, 2, 3]
-print tab
+emails = []
+50.times do |j|
+	if j < 9
+		emails.append "jean.dupont.0#{j + 1}@email.fr"
+	else 
+		emails.append "jean.dupont.#{j + 1}@email.fr"
+	end 
+end 
+puts (emails)
